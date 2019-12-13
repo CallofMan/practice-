daab67aaf27b1d0f1a7a66519fe276b0541ad891
Ivan <ivan.verbitsk@yandex.ru>
2019-12-13 18:02:51 +0300

    Удаление лишнего

--- messages.php
+++ messages.php
@@ -9,7 +9,6 @@
         $id_user = $_SESSION['id'];
         $datetime = date("Y-m-d H:i:s");
 
-        echo $message . "    " . $role_user . "    " . $id_user . "     " . $datetime;
         $queryAdd = mysqli_query($link, "INSERT INTO messages (id_message, id_user, id_room, text_message, date) 
         VALUES (NULL, $id_user, 1, '$message', '$datetime')");
     }

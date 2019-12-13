5d5a5b150edd2f7fb2abb907ccb90e8ebf47aadf
Ivan <ivan.verbitsk@yandex.ru>
2019-12-13 16:01:05 +0300

    Косметические фиксы

--- forum.php
+++ forum.php
@@ -8,36 +8,34 @@
     <link rel="stylesheet" href="styles/style_forum.css">
 </head>
 <body>
-    <div id="chat">
-        <section>
+        <div id="chat">
+            <section>
 
-        </section>
+            </section>
 
-        <form method="GET" id="form_message">
-            <textarea name="message" placeholder="Введите сообщение" require id="message"></textarea>
-            <input type="submit" name="send" value="Send" id="send">
-        </form>
-    </div>
-    
-
-    <div id="right">
-        <nav>
-            <a href="index.php">Главная</a>
-            <a href="">Личный кабинет</a>
-            <a href="">Телефонный справочник</a>
-            <a href="">Новости</a>
-        </nav>
-        <aside>
-            <p>Комната</p>
-            <p>Комната</p>
-            <p>Комната</p>
-            <p>Комната</p>
-        </aside>
-        <div id="name">
-            <h3>Васян Васяныч</h3>
+            <form method="GET" id="form_message">
+                <textarea name="message" placeholder="Введите сообщение" require id="message"></textarea>
+                <input type="submit" name="send" value="Send" id="send">
+            </form>
         </div>
-    </div>
+        
 
-
+        <div id="right">
+            <nav>
+                <a href="index.php">Главная</a>
+                <a href="">Личный кабинет</a>
+                <a href="">Телефонный справочник</a>
+                <a href="">Новости</a>
+            </nav>
+            <aside>
+                <p>Комната</p>
+                <p>Комната</p>
+                <p>Комната</p>
+                <p>Комната</p>
+            </aside>
+            <div id="name">
+                <h3>Васян Васяныч</h3>
+            </div>
+        </div>
 </body>
 </html>

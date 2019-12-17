3315d10d0a304376a73fbf2b9dbc05df738bd4ab
Sof7ik (Leonid Bychkov) <bychkov.l47@mail.ru>
2019-12-18 02:31:20 +0300

    Плавная прокрутка до карты

--- index.php
+++ index.php
@@ -85,6 +85,17 @@
 			<a href="#footerMap">Как добраться</a>
 			<a href="#">О компании</a>
 			<a href="allNews.php">Все новости</a>
+			
+			<script src="jquery-3.3.1.min.js"></script> <!--ща пойдет JS, который я скопипастил из кода ведущего, хз ваще как оно работает. Первый JS - плавный скролл-->
+			<script>
+			$(function(){
+				$('a[href^="#"]').click(function(){
+					var target = $(this).attr('href');
+					$('html, body').animate({scrollTop: $(target).offset().top}, 800);//800 - длительность скроллинга в мс
+					return false; 
+				}); 
+			});
+			</script>
 
 			<?php
 				if($_SESSION['role'] == 1)

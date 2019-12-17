3be02af3635a45a427cdab23bf85d41f07f2efc6
Ivan <ivan.verbitsk@yandex.ru>
2019-12-17 19:38:34 +0300

    Добавил ограничение на удаление по ролям

--- messagesUpload.php
+++ messagesUpload.php
@@ -26,13 +26,29 @@ for($i = 0; $i < $lengthArray; $i++)
     $queryNameUserM = mysqli_query($link, "SELECT first_name, second_name FROM users WHERE id_user = $idUserM");
     $nameUserM = $queryNameUserM->fetch_assoc();
 
-    // Если сообщение ввели вы, то оно будет справа и другого цвета, если не вы, то слева
-    if ($idUserM == $idUserFromSession)
+    if($_SESSION['role'] == 1)
     {
-        echo "<div class='message_block' id='message" . $messagesArray[$i][0] . "' style='align-self: flex-end;'><h4>" . $nameUserM['first_name'] . " " . $nameUserM['second_name'] . "</h4> <p class='message_content' style='background-color: rgba(0, 169, 251, 0.3); color:white; opacity: 1;'> " . $messagesArray[$i][2] . " </p> <div class='deleteAndDate'> <p class='delete_message' id='" . $messagesArray[$i][0] . "'>удалить</p> <h6>" . $messagesArray[$i][3] . "</h6></div> </div>";
+        // Если сообщение ввели вы, то оно будет справа и другого цвета, если не вы, то слева
+        if ($idUserM == $idUserFromSession)
+        {
+            echo "<div class='message_block' id='message" . $messagesArray[$i][0] . "' style='align-self: flex-end;'><h4>" . $nameUserM['first_name'] . " " . $nameUserM['second_name'] . "</h4> <p class='message_content' style='background-color: rgba(0, 169, 251, 0.3); color:white; opacity: 1;'> " . $messagesArray[$i][2] . " </p> <div class='deleteAndDate'> <p class='delete_message' id='" . $messagesArray[$i][0] . "'>удалить</p> <h6>" . $messagesArray[$i][3] . "</h6></div> </div>";
+        }
+        else
+        {
+            echo "<div class='message_block' id='message" . $messagesArray[$i][0] . "' style='align-self: flex-start;'><h4>" . $nameUserM['first_name'] . " " . $nameUserM['second_name'] . "</h4> <p class='message_content'> " . $messagesArray[$i][2] . " </p> <div class='deleteAndDate'> <p class='delete_message' id='" . $messagesArray[$i][0] . "'>удалить</p> <h6>" . $messagesArray[$i][3] . "</h6></div> </div>";
+        }
     }
     else
     {
-        echo "<div class='message_block' id='message" . $messagesArray[$i][0] . "' style='align-self: flex-start;'><h4>" . $nameUserM['first_name'] . " " . $nameUserM['second_name'] . "</h4> <p class='message_content'> " . $messagesArray[$i][2] . " </p> <div class='deleteAndDate'> <p class='delete_message' id='" . $messagesArray[$i][0] . "'>удалить</p> <h6>" . $messagesArray[$i][3] . "</h6></div> </div>";
+        // Если сообщение ввели вы, то оно будет справа и другого цвета, если не вы, то слева
+        if ($idUserM == $idUserFromSession)
+        {
+            echo "<div class='message_block' id='message" . $messagesArray[$i][0] . "' style='align-self: flex-end;'><h4>" . $nameUserM['first_name'] . " " . $nameUserM['second_name'] . "</h4> <p class='message_content' style='background-color: rgba(0, 169, 251, 0.3); color:white; opacity: 1;'> " . $messagesArray[$i][2] . " </p> <h6>" . $messagesArray[$i][3] . "</h6> </div>";
+        }
+        else
+        {
+            echo "<div class='message_block' id='message" . $messagesArray[$i][0] . "' style='align-self: flex-start;'><h4>" . $nameUserM['first_name'] . " " . $nameUserM['second_name'] . "</h4> <p class='message_content'> " . $messagesArray[$i][2] . " </p> <h6>" . $messagesArray[$i][3] . "</h6> </div>";
+        }
     }
+    
 }  

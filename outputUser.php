04c391e553818b138306bafc70cb98f06d04b735
Ivan <ivan.verbitsk@yandex.ru>
2019-12-18 03:55:48 +0300

    Флекс и адаптация личного кабинета

--- outputUser.php
+++ outputUser.php
@@ -13,10 +13,25 @@ if ($selectInfo) {
     for ($i = 0; $i < mysqli_num_rows($selectInfo); $i++) {
         $selectInfoResult = mysqli_fetch_assoc($selectInfo);
     }
-    echo "<li>".$selectInfoResult['first_name']." ".$selectInfoResult['second_name']."</li> 
-    <li>".$selectInfoResult['position']."</li> 
-    <li>".$selectInfoResult['login']."</li> 
-    <li>".$selectInfoResult['password']."</li> 
-    <li>".$selectInfoResult['mail']."</li>";
+    echo "
+    <li>
+        <p class='userInfoDescripton'>Фамилия и Имя сотрудника</p>
+        <p class='innerUserInfo'>".$selectInfoResult['first_name'] . " " . $selectInfoResult['second_name']."</p>
+    </li> 
+
+    <li>
+        <p class='userInfoDescripton'>Должность сотрудника</p>
+        <p class='innerUserInfo'>".$selectInfoResult['position']."</p>
+    </li>
+
+    <li> 
+        <p class='userInfoDescripton'>Логин сотрудника для внутренней сиситемы</p>
+        <p class='innerUserInfo'>". $selectInfoResult['login'] ."</p>
+    </li>
+
+    <li>
+        <p class='userInfoDescripton'>Email сотрудника</p>
+        <p class='innerUserInfo'>".$selectInfoResult['mail']."</p>
+    </li>";
 }
 ?>

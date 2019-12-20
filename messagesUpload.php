8c969ad2ec8bb82ba6a4b64d0efc36b40ad96a18
Ivan <ivan.verbitsk@yandex.ru>
2019-12-21 00:26:35 +0300

    Рефакторинг кода

--- messagesUpload.php
+++ messagesUpload.php
@@ -31,11 +31,27 @@ for($i = 0; $i < $lengthArray; $i++)
         // Если сообщение ввели вы, то оно будет справа и другого цвета, если не вы, то слева
         if ($idUserM == $idUserFromSession)
         {
-            echo "<div class='message_block' id='message" . $messagesArray[$i][0] . "' style='align-self: flex-end;'><a class='logo' href='personalAccount.php' id='" . $idUserM . "'>" . $nameUserM['first_name'] . " " . $nameUserM['second_name'] . "</a> <p class='message_content' style='background-color: rgba(0, 169, 251, 0.3); color:white; opacity: 1;'> " . $messagesArray[$i][2] . " </p> <div class='deleteAndDate'> <p class='delete_message' id='" . $messagesArray[$i][0] . "'>удалить</p> <h6>" . $messagesArray[$i][3] . "</h6></div> </div>";
+            echo "
+            <div class='message_block' id='message" . $messagesArray[$i][0] . "' style='align-self: flex-end;'>
+                <a class='logo' href='personalAccount.php' id='" . $idUserM . "'>" . $nameUserM['first_name'] . " " . $nameUserM['second_name'] . "</a> 
+                <p class='message_content' style='background-color: rgba(0, 169, 251, 0.3); color:white; opacity: 1;'> " . $messagesArray[$i][2] . " </p> 
+                <div class='deleteAndDate'> 
+                    <p class='delete_message' id='" . $messagesArray[$i][0] . "'>удалить</p> 
+                    <h6>" . $messagesArray[$i][3] . "</h6>
+                </div> 
+            </div>";
         }
         else
         {
-            echo "<div class='message_block' id='message" . $messagesArray[$i][0] . "' style='align-self: flex-start;'><a class='logo' href='personalAccount.php' id='" . $idUserM . "'>" . $nameUserM['first_name'] . " " . $nameUserM['second_name'] . "</a> <p class='message_content'> " . $messagesArray[$i][2] . " </p> <div class='deleteAndDate'> <p class='delete_message' id='" . $messagesArray[$i][0] . "'>удалить</p> <h6>" . $messagesArray[$i][3] . "</h6></div> </div>";
+            echo "
+            <div class='message_block' id='message" . $messagesArray[$i][0] . "' style='align-self: flex-start;'>
+                <a class='logo' href='personalAccount.php' id='" . $idUserM . "'>" . $nameUserM['first_name'] . " " . $nameUserM['second_name'] . "</a> 
+                <p class='message_content'> " . $messagesArray[$i][2] . " </p> 
+                <div class='deleteAndDate'> 
+                    <p class='delete_message' id='" . $messagesArray[$i][0] . "'>удалить</p> 
+                    <h6>" . $messagesArray[$i][3] . "</h6>
+                </div> 
+            </div>";
         }
     }
     else
@@ -43,11 +59,21 @@ for($i = 0; $i < $lengthArray; $i++)
         // Если сообщение ввели вы, то оно будет справа и другого цвета, если не вы, то слева
         if ($idUserM == $idUserFromSession)
         {
-            echo "<div class='message_block' id='message" . $messagesArray[$i][0] . "' style='align-self: flex-end;'><a class='logo' href='#' id='" . $idUserM . "'>" . $nameUserM['first_name'] . " " . $nameUserM['second_name'] . "</a> <p class='message_content' style='background-color: rgba(0, 169, 251, 0.3); color:white; opacity: 1;'> " . $messagesArray[$i][2] . " </p> <h6>" . $messagesArray[$i][3] . "</h6> </div>";
+            echo "
+            <div class='message_block' id='message" . $messagesArray[$i][0] . "' style='align-self: flex-end;'>
+                <a class='logo' href='#' id='" . $idUserM . "'>" . $nameUserM['first_name'] . " " . $nameUserM['second_name'] . "</a> 
+                <p class='message_content' style='background-color: rgba(0, 169, 251, 0.3); color:white; opacity: 1;'> " . $messagesArray[$i][2] . " </p> 
+                <h6>" . $messagesArray[$i][3] . "</h6> 
+            </div>";
         }
         else
         {
-            echo "<div class='message_block' id='message" . $messagesArray[$i][0] . "' style='align-self: flex-start;'><a class='logo' href='#' id='" . $idUserM . "'>" . $nameUserM['first_name'] . " " . $nameUserM['second_name'] . "</a> <p class='message_content'> " . $messagesArray[$i][2] . " </p> <h6>" . $messagesArray[$i][3] . "</h6> </div>";
+            echo "
+            <div class='message_block' id='message" . $messagesArray[$i][0] . "' style='align-self: flex-start;'>
+                <a class='logo' href='#' id='" . $idUserM . "'>" . $nameUserM['first_name'] . " " . $nameUserM['second_name'] . "</a> 
+                <p class='message_content'> " . $messagesArray[$i][2] . " </p> 
+                <h6>" . $messagesArray[$i][3] . "</h6> 
+            </div>";
         }
     }
     
